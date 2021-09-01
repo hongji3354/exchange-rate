27090b87d21e3f66498a6dd15dd1bd0eb69d10ce
JiHun Hong <hongji3354@gmail.com>
2021-09-02 00:05:23 +0900

    Project Init

---
+++ src/main/java/dev/jihun/ExchangeRateCalculationApplication.java
@@ -0,0 +1,13 @@
+package dev.jihun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateCalculationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeRateCalculationApplication.class, args);
+    }
+
+}
