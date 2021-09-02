f4bd343713af7c97ad9f187740385a1b47e6bbf6
JiHun Hong <hongji3354@gmail.com>
2021-09-02 23:01:42 +0900

    API의 URL이 누락되어 추가

--- src/main/java/dev/jihun/domain/exchangerate/api/ExchangeRateApi.java
+++ src/main/java/dev/jihun/domain/exchangerate/api/ExchangeRateApi.java
@@ -6,6 +6,8 @@ import dev.jihun.domain.exchangerate.dto.ReceivedAmountCalculationRequest;
 import dev.jihun.domain.exchangerate.dto.ReceivedAmountCalculationResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +17,12 @@ public class ExchangeRateApi {
 
     private final ExchangeRateApplication exchangeRateApplication;
 
+    @GetMapping("/exchange-rate")
     public ResponseEntity<ExchangeRateInformationResponse> exchangeRateInformation() {
         return ResponseEntity.ok(exchangeRateApplication.getExchangeRateInformation());
     }
 
+    @PostMapping("/receive-amount/calculation")
     public ResponseEntity<ReceivedAmountCalculationResponse> receivedAmountCalculation(@RequestBody ReceivedAmountCalculationRequest request) {
         return ResponseEntity.ok(exchangeRateApplication.receivedAmountCalculation(request));
     }
