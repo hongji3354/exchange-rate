44f8197297f9b656ed6a1c70ee44e68c006fabac
JiHun Hong <hongji3354@gmail.com>
2021-09-02 21:42:10 +0900

    currencylayer API 통신을 위한 RestTemplate 설정 추가

---
+++ src/main/java/dev/jihun/global/config/resttemplate/RestTemplateConfig.java
@@ -0,0 +1,27 @@
+package dev.jihun.global.config.resttemplate;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@RequiredArgsConstructor
+public class RestTemplateConfig {
+
+    private final RestTemplateBuilder restTemplateBuilder;
+
+    @Bean
+    public RestTemplate currencylayerExchangeRateTemplate() {
+        return restTemplateBuilder.rootUri("http://apilayer.net")
+            .requestFactory(() -> new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()))
+            .interceptors(new RestTemplateClientInterceptor())
+            .errorHandler(new CurrencylayerExchangeRateErrorHandler())
+            .build();
+
+
+    }
+}
